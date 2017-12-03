5322291bb03b2d9afa3a2edb0f1db102e312bedb
Gideon Chia <gidchia@uw.edu>
2017-12-03 15:21:05 -0800

    edited data

---
+++ Gid's Functions.R
@@ -0,0 +1,7 @@
+setwd("~/Desktop/INFO201/Assignments/FinalProject-TuesdayNightWarriors/Data")
+
+library(dplyr)
+library(ggplot2)
+
+PhoneData <- read.csv("phone_dataset.csv", stringsAsFactors = FALSE)
+

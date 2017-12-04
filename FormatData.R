6da58be9628a87dc18a8a7ae46f11119ffa01ace
taingmat-1574893 <taingmat@uw.edu>
2017-12-04 02:13:27 -0800

    Made values compareable numbers

--- FormatData.R
+++ FormatData.R
@@ -9,17 +9,22 @@ DATA <- read.csv("Data/phone_dataset.csv", quote = "", row.names = NULL, strings
 
 ModifiedData <- DATA[, -which(names(DATA) %in% c("status", "weight_oz"))]
 
-PrimaryJustMP <- stri_extract_first_regex(ModifiedData$primary_camera, "[0.0-9.0]+")
+PrimaryJustMP <- as.numeric(stri_extract_first_regex(ModifiedData$primary_camera, "[0.0-9.0]+"))
 
-SecondaryJustMP <- stri_extract_first_regex(ModifiedData$secondary_camera, "[0.0-9.0]+")
+SecondaryJustMP <- as.numeric(stri_extract_first_regex(ModifiedData$secondary_camera, "[0.0-9.0]+"))
 
-CpuGHz <- stri_extract_first_regex(ModifiedData$CPU, "[0.0-9.0]+")
+CpuGHz <- as.numeric(stri_extract_first_regex(ModifiedData$CPU, "[0.0-9.0]+"))
 
-MemoryCard <- stri_extract_first_regex(ModifiedData$memory_card, "[0.0-9.0]+")
+MemoryCard <- as.numeric(stri_extract_first_regex(ModifiedData$memory_card, "[0.0-9.0]+"))
 
-BatteryMPH <- stri_extract_first_regex(ModifiedData$battery, "[0.0-9.0]+") 
+BatteryMPH <- as.numeric(stri_extract_first_regex(ModifiedData$battery, "[0.0-9.0]+"))
 
-MoreModifiedData <- ModifiedData %>%  mutate("Primary Camera MP"= PrimaryJustMP) %>% mutate ("Secondry Camera MP" = SecondaryJustMP) %>% mutate("CPU GHz"=CpuGHz) %>% mutate("Memory Card GB"=MemoryCard) %>% mutate("Battery MPH"=BatteryMPH)
+phone_data <- ModifiedData %>%  
+  mutate("Primary_Camera_MP"= PrimaryJustMP) %>% 
+  mutate ("Secondry_Camera_MP" = SecondaryJustMP) %>% 
+  mutate("CPU_GHz"=CpuGHz) %>% 
+  mutate("Memory_Card_GB"=MemoryCard) %>% 
+  mutate("Battery_MPH"=BatteryMPH)
 
 
 

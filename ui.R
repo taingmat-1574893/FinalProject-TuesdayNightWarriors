021c30fe6a75b10525ae3c475b5b4fa035940419
Kyrabautista <kyrabb@uw.edu>
2017-12-03 17:17:08 -0800

    added dashboard widgets

--- ui.R
+++ ui.R
@@ -3,77 +3,96 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 library(plotly)
+library(shinydashboard)
+library(shinyjs)
 
-#Data setup
+# #Data setup
 phone_data <- read.csv("Data/phone_dataset.csv", quote = "", row.names = NULL, stringsAsFactors = FALSE)
 
-# Start of UI
+#Start of UI
 shinyUI(fluidPage(
-  
+
   # Application title
   titlePanel("TruePhone"),
-  
-  #Sidebar that contains both widgets on the left side 
-  sidebarLayout(
-    sidebarPanel(
-      
-      selectizeInput("brand_list", "Brand", choices = list("a", "b", "c", "d"), 
-                     multiple = TRUE, 
-                     options = list(maxItems = 4), 
-                     selected = list("a", "b", "c")),
-      selectizeInput("displaytype", "Display Type", choices = list("a", "b", "c", "d"), 
-                     multiple = TRUE, 
-                     options = list(maxItems = 4), 
-                     selected = list("a", "b", "c")),
-      selectizeInput("os", "Overhead OS", choices = list("a", "b", "c", "d"), 
-                     multiple = TRUE, 
-                     options = list(maxItems = 4), 
-                     selected = list("a", "b", "c")),
-      selectizeInput("sim", "Sim", choices = list("a", "b", "c", "d"), 
-                     multiple = TRUE, 
-                     options = list(maxItems = 4), 
-                     selected = list("a", "b", "c")),
-      selectizeInput("bluetooth", "Bluetooth", choices = list("a", "b", "c", "d"), 
-                     multiple = TRUE, 
-                     options = list(maxItems = 4), 
-                     selected = list("a", "b", "c")),
-      selectizeInput("usb", "USB", choices = list("a", "b", "c", "d"), 
-                     multiple = TRUE, 
-                     options = list(maxItems = 4), 
-                     selected = list("a", "b", "c")),
-      #
-      sliderInput("date", "Announced Date:",
-                  min = 1, max = 1000, # change to dates
-                  value = c(200,500)),
-      sliderInput("cpu", "CPU:",
-                  min = 0, max = 8,
-                  value = 0),
-      sliderInput("internalmem", "Internal Memory:",
-                  min = 1, max = 1000, # change to dates
-                  value = c(200,500)),
-      sliderInput("ram", "Ram:",
-                  min = 1, max = 1000, # change to dates
-                  value = c(200,500)),
-      sliderInput("primarymb", "Primary-MP:",
-                  min = 1, max = 1000, # change to dates
-                  value = c(200,500)),
-      sliderInput("secondarymb", "Secondary-MP:",
-                  min = 1, max = 1000, # change to dates
-                  value = c(200,500)),
-      sliderInput("price", "Approx. Price:",
-                  min = 1, max = 1000, # change to dates
-                  value = c(200,500)),
-    
-      #Allows user to select a trait to compare by 
-      selectizeInput("boolean", "Boolean filter", choices = list("Expandable memory card", "Sensors", "GPS", "NFC", "Loud speaker", "Audio jack"), 
-                     multiple = TRUE, 
-                     options = list(maxItems = 4), 
-                     selected = list("Expandable memory card"))
-      
 
-    ),
-  
-    mainPanel(
+  dashboardPage(
+     skin = "purple",
+     
+     dashboardHeader(
+       title = "Phone finder"),
+     dashboardSidebar(
+        useShinyjs(),
+        width = 325,
+        selectizeInput("brand_list", "Brand:", choices = list("a", "b", "c", "d"),
+        multiple = TRUE,
+        options = list(maxItems = 4),
+        selected = list("a", "b", "c")
+        ), 
+        
+        useShinyjs(),
+        selectizeInput("display_type", "Display type:", choices = list("a", "b", "c", "d"),
+                       multiple = TRUE,
+                       options = list(maxItems = 4),
+                       selected = list("a", "b", "c")
+        ),
+        selectizeInput("os", "Overhead OS:", choices = list("a", "b", "c", "d"),
+                       multiple = TRUE,
+                       options = list(maxItems = 4),
+                       selected = list("a", "b", "c")
+        ), 
+        selectizeInput("sim", "Sim:", choices = list("a", "b", "c", "d"),
+                       multiple = TRUE,
+                       options = list(maxItems = 4),
+                       selected = list("a", "b", "c")
+        ),
+        selectizeInput("bluetooth", "Bluetooth", choices = list("a", "b", "c", "d"),
+                       multiple = TRUE,
+                       options = list(maxItems = 4),
+                       selected = list("a", "b", "c")),
+        selectizeInput("usb", "USB", choices = list("a", "b", "c", "d"),
+                       multiple = TRUE,
+                       options = list(maxItems = 4),
+                       selected = list("a", "b", "c")),
+        sliderInput("date", "Announced Date:",
+                    min = 1, max = 1000, # change to dates
+                    value = c(200,500)),
+        sliderInput("cpu", "CPU:",
+                    min = 0, max = 8,
+                    value = 0),
+        sliderInput("internalmem", "Internal Memory:",
+                    min = 1, max = 1000, # change to dates
+                    value = c(200,500)),
+        sliderInput("ram", "Ram:",
+                    min = 1, max = 1000, # change to dates
+                    value = c(200,500)),
+        sliderInput("primarymb", "Primary-MP:",
+                    min = 1, max = 1000, # change to dates
+                    value = c(200,500)),
+        sliderInput("secondarymb", "Secondary-MP:",
+                    min = 1, max = 1000, # change to dates
+                    value = c(200,500)),
+        sliderInput("price", "Approx. Price:",
+                    min = 1, max = 1000, # change to dates
+                    value = c(200,500))
+      
+     ),
+     
+     dashboardBody(
+         # this is now the main panel                         
+        #actionButton("showSidebar", "Show sidebar"),
+        #actionButton("hideSidebar", "Hide sidebar")
+      )
     )
   )
-))
+)
+
+server <-function(input, output) {
+  observeEvent(input$showSidebar, {
+    shinyjs::removeClass(selector = "body", class = "sidebar-collapse")
+  })
+  observeEvent(input$hideSidebar, {
+    shinyjs::addClass(selector = "body", class = "sidebar-collapse")
+  })
+}
+
+#shinyApp(ui, server)

a5add3fb9224498bec3a64e96f9f66d4f43635c7
Micxey <mikiaslema@D-10-18-241-79.dhcp4.washington.edu>
2017-12-03 17:10:49 -0800

    More modified data

--- FormatData.R
+++ FormatData.R
@@ -1,6 +1,14 @@
 library(dplyr)
+library(stringi)
 
 #Format data
-DATA <- read.csv("/Users/mikiaslema/Documents/INFO201/Projects/FinalProject/FinalProject-TuesdayNightWarriors/Data/phone_dataset.csv", quote = "", row.names = NULL, stringsAsFactors = FALSE)
 
-ModifiedData<- DATA[, -which(names(DATA) %in% c("status", "weight_oz"))]
+#DATA <- read.csv("/Users/mikiaslema/Documents/INFO201/Projects/FinalProject/FinalProject-TuesdayNightWarriors/Data/phone_dataset.csv", quote = "", row.names = NULL, stringsAsFactors = FALSE)
+
+DATA <- read.csv("/Data/phone_dataset.csv", quote = "", row.names = NULL, stringsAsFactors = FALSE)
+
+ModifiedData <- DATA[, -which(names(DATA) %in% c("status", "weight_oz"))]
+
+JustMP <- stri_extract_first_regex(ModifiedData$primary_camera, "[0-9]+")
+
+MoreModifiedData <- ModifiedData %>%  mutate("Primary Camera MP"=JustMP)

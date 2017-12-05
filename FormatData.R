d894e2a6f438e4baba0c31bc3da51aaedd0bd319
taingmat-1574893 <taingmat@uw.edu>
2017-12-04 17:00:48 -0800

    Fixed issues with internal memory

--- FormatData.R
+++ FormatData.R
@@ -16,27 +16,25 @@ GreaterThan50NA <- function(number) {
 
 DATA <- read.csv("Data/phone_dataset.csv", quote = "", row.names = NULL, stringsAsFactors = FALSE)
 
+#use baseline r regx 
 ModifiedData <- DATA[, -which(names(DATA) %in% c("status", "weight_oz"))] %>% 
   mutate(CPU = gsub('4x', "", CPU), CPU = gsub('2x', "", CPU), CPU = gsub('3x', "", CPU))
   
 PrimaryJustMP <- as.numeric(stri_extract_first_regex(ModifiedData$primary_camera, "[0.0-9.0]+"))
-
 SecondaryJustMP <- as.numeric(stri_extract_first_regex(ModifiedData$secondary_camera, "[0.0-9.0]+"))
-
 CpuGHz <- as.numeric(stri_extract_first_regex(ModifiedData$CPU, "[0.0-9.0]+"))
-
 MemoryCard <- as.numeric(stri_extract_first_regex(ModifiedData$memory_card, "[0.0-9.0]+"))
-
 BatteryMPH <- as.numeric(stri_extract_first_regex(ModifiedData$battery, "[0.0-9.0]+"))
-
 internalMem <- as.numeric(stri_extract_last_regex(ModifiedData$internal_memory, "[0.0-9.0]+"))
-
+#Ram <- as.numeric(stri_extract_first_regex(ModifiedData$RAM, "[0.0-9.0]+"))
+#Applying
 phone_data <- ModifiedData  %>%  
-  mutate("Primary_Camera_MP"= PrimaryJustMP) %>% 
-  mutate ("Secondry_Camera_MP" = SecondaryJustMP) %>% 
   mutate("CPU_GHz"=CpuGHz) %>% 
   mutate("Memory_Card_GB"=MemoryCard) %>% 
-  mutate("Battery_MPH"=BatteryMPH) 
-
-test <- ModifiedData %>% 
-  mutate("Primary_Camera_MP" = PrimaryJustMP)
+  mutate("Battery_MPH"=BatteryMPH) %>% 
+  mutate("Primary_Camera_MP" = ifelse(PrimaryJustMP > 50, NA, PrimaryJustMP)) %>%
+  mutate("Secondary_Camera_MP" = ifelse(SecondaryJustMP > 20, NA, SecondaryJustMP)) %>%
+  mutate("Internal-Memory" = internalMem) %>%
+  mutate("Ram_GB" = Ram) 
+phone_data[phone_data==""]<-NA
+phone_data[phone_data=="N/A"]<-NA

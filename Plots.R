04c62129bfac82d3d6bbb9c1c155b5f3bfb41de9
taingmat-1574893 <taingmat@uw.edu>
2017-12-05 23:53:41 -0800

    Added Y-labels

--- Plots.R
+++ Plots.R
@@ -3,19 +3,6 @@ library(ggplot2)
 source("FormatData.R")
 
 
-# PhoneModelList = list(
-#    "iPhone 7 Plus",
-#    "iPhone 7",
-#    "iPhone SE"
-#   )
-# 
-# ListofSpecs = list(
-#     "RAM" = 'Ram_GB',
-#     "Battery" = 'Battery_MPH',
-#     "CPU" = 'CPU_GHz',
-#     "Internal Memory" = 'Internal Memory'
-#   
-#   )
 multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
   require(grid)
   
@@ -61,19 +48,19 @@ if(length(ListofSpecs) >= 1){
   x <- ggplot(phoneDF,aes(x=model,y= eval(parse(text=ListofSpecs[1])))) +
       geom_bar(stat="identity",aes(fill= model)) + 
     coord_flip() +
-    labs(x = "Please work!!!", y = ListofSpecs[1]) 
+    labs(x = "Model", y = ListofSpecs[1]) 
     }
 if(length((ListofSpecs) >= 2)){
   y <- ggplot(phoneDF,aes(x=model,y= eval(parse(text=ListofSpecs[2])))) +
     geom_bar(stat="identity",aes(fill= model)) + 
     coord_flip() +
-    labs(x = "I'm updated", y = ListofSpecs[2]) 
+    labs(x = "Model", y = ListofSpecs[2]) 
   }
  if (length((ListofSpecs) >= 3)){
    z <- ggplot(phoneDF,aes(x=model,y= eval(parse(text=ListofSpecs[3])))) +
      geom_bar(stat="identity",aes(fill= model)) + 
      coord_flip() +
-     labs(x = "Please work!!!", y = ListofSpecs[3]) 
+     labs(x = "Model", y = ListofSpecs[3]) 
  }
   if (length(ListofSpecs) == 1){
   return (multiplot(x))
@@ -84,21 +71,7 @@ if(length((ListofSpecs) >= 2)){
   if (length(ListofSpecs) == 3){
     return (multiplot(x,y,z))
   }
-  #return(multiplot(x, y, z))
 }
-
-
-
-# TPhoneModelList = c(
-#   "iPhone 7 Plus",
-#   "iPhone 7",
-#   "iPhone SE"
-# )
-# 
-# TListofSpecs = c(
-#     'RAM',
-#     'battery',
-#     'CPU')
 
 TableOG <- read.csv("Data/phone_dataset - original.csv",quote = "", row.names = NULL, stringsAsFactors = FALSE)
 
